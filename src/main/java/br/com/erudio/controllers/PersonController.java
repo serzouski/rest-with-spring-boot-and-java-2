d8536b73d38614458ce4d43b71395b949ad2df04
serzouski <xande_serzouski@hotmail.com>
2023-08-17 08:33:18 -0300

    arrumei tudo

--- src/main/java/br/com/erudio/controllers/PersonController.java
+++ src/main/java/br/com/erudio/controllers/PersonController.java
@@ -8,9 +8,6 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 
-import br.com.erudio.exceptions.UnsupportedMathOperationException;
-
-import java.awt.*;
 import java.util.List;
 
 
@@ -28,7 +25,7 @@ public class PersonController {
 	}
     @GetMapping(value = "/{id}",
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public Person findById(@PathVariable(value = "id") String id) throws Exception {
+    public Person findById(@PathVariable(value = "id") Long id) throws Exception {
         return service.findById(id);
 
     }
@@ -47,7 +44,7 @@ public class PersonController {
 
     }
     @DeleteMapping(value = "/{id}")
-    public void delete(@PathVariable(value = "id") String id) {
+    public void delete(@PathVariable(value = "id") Long id) {
         service.delete(id);
 
     }
--- src/main/java/br/com/erudio/services/PersonServices.java
+++ src/main/java/br/com/erudio/services/PersonServices.java
@@ -1,6 +1,9 @@
 package br.com.erudio.services;
 
+import br.com.erudio.exceptions.ResourceNotFoundException;
 import br.com.erudio.model.Person;
+import br.com.erudio.repositories.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -11,56 +14,46 @@ import java.util.logging.Logger;
 @Service
 public class PersonServices {
 
-    private final AtomicLong counter = new AtomicLong();
+
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
 
+    @Autowired
+    PersonRepository repository;
     public List<Person> findAll() {
         logger.info("Finding all people!");
-        List<Person> people = new ArrayList<>();
-        for (int i = 0; i < 8 ; i++) {
-            Person person = mockPerson(i);
-            people.add(person);
 
-        }
-        return people;
+        return repository.findAll();
     }
 
 
-    public Person findById(String id) {
+    public Person findById(Long id) {
 
         logger.info("Finding one person!");
 
-        Person person = new Person();
-        person.setId(counter.incrementAndGet());
-        person.setFirstName("Alexandre");
-        person.setLastName("Serzouski");
-        person.setAddress("Ponta Grossa");
-        person.setGender("Male");
-        return person;
+
+        return repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
     }
 
     public Person create(Person person) {
         logger.info("Creating one person!");
 
-        return person;
+        return repository.save(person);
     }
     public Person update(Person person) {
         logger.info("Updating one person!");
-
-        return person;
+        var entity = repository.findById(person.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+        entity.setFirstName(person.getFirstName());
+        entity.setLastName(person.getLastName());
+        entity.setAddress(person.getAddress());
+        entity.setGender(person.getGender());
+        return repository.save(person);
     }
-    public void delete(String id) {
+    public void delete(Long id) {
         logger.info("Deleting one person!");
-
-    }
-    private Person mockPerson(int i) {
-
-        Person person = new Person();
-        person.setId(counter.incrementAndGet());
-        person.setFirstName("Person name " + (i+1));
-        person.setLastName("Last name " + (i+1));
-        person.setAddress("Some address in Brazil " + (i+1));
-        person.setGender("Male");
-        return person;
+        var entity = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+        repository.delete(entity);
     }
 }
